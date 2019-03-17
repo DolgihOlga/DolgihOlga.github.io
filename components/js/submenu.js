812b0d3801d5c750a44daa5818fb4e317a6e0d61
Olga Dolgih <dolgiholgadol@gmail.com>
2019-03-17 14:09:56 +0300

    add changes in html files, add review.js file

--- components/js/submenu.js
+++ components/js/submenu.js
@@ -1,8 +1,8 @@
 function ShowSub() {
     var display = document.getElementById('menu');
     display.style.display = 'block';
-
 }
+
 function HideSub() {
     var display = document.getElementById('menu');
     display.style.display = 'none';
